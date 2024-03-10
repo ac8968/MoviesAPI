5950aaa3d9af365da3cd189d58442fbb0cfc5db6
Avinash Chavan <ac8968@gmail.com>
2024-03-11 00:18:31 +0530

    Working crud operation for movies

--- MoviesAPI/Controllers/MoviesController.cs
+++ MoviesAPI/Controllers/MoviesController.cs
@@ -1,47 +1,58 @@
-﻿namespace MoviesAPI.Controllers
+﻿using Azure.Core;
+using System.Net.Http.Headers;
+
+namespace MoviesAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class MoviesController : ControllerBase
+    public class MoviesController(IMovies movies) : ControllerBase
     {
-        public MoviesController()
-        {
-        }
+        private readonly IMovies movies = movies;
+        private Response response = new();
 
         [HttpGet]
         [Route("/GetMovies")]
         public IActionResult Get()
         {
             Log.Information("Get All Movies called");
-            return Ok(new { id = 1 });
+            response = movies.GetAllMovies();
+            return StatusCode((int)response.StatusCode,response.Content);
         }
 
         [HttpGet]
         [Route("/GetMovieById/{id}")]
         public IActionResult Get(int id)
         {
-            return Ok(id);
+            Log.Information("Get Movies by id called");
+            response = movies.GetMovieById(id);
+            return StatusCode((int)response.StatusCode, response.Content);
         }
 
         [HttpPost]
         [Route("/AddMovie")]
         public IActionResult Add(Movies movie)
         {
-            return Ok(new { id = 1 });
+            Log.Information("Add Movies called");
+            response = movies.AddMovie(movie);
+            return StatusCode((int)response.StatusCode, response.Content);
         }
 
         [HttpPut]
         [Route("/UpdateMovie/{id}")]
-        public IActionResult Update(int id, [FromBody] Movies request ) 
+        public IActionResult Update(int id, [FromBody] Movies request)
         {
-            return Ok();
+            Log.Information("Update Movies called");
+            response = movies.UpdateMovie(id, request);
+            return StatusCode((int)response.StatusCode, response.Content);
         }
 
         [HttpDelete]
         [Route("/DeleteMovie/{id}")]
-        public IActionResult Delete(int id) 
+        public IActionResult Delete(int id)
         {
-            return Ok();
+            Log.Information("Delete Movies called");
+            response = movies.DeleteMovie(id);
+            return StatusCode((int)response.StatusCode, response.Content);
         }
     }
 }
--- MoviesAPI/Services/MoviesService.cs
+++ MoviesAPI/Services/MoviesService.cs
@@ -3,7 +3,7 @@
     public class MoviesService : IMovies
     {
         private readonly MoviesDbCotext _dbContext;
-        private Response response;
+        private readonly Response response = new();
 
         public MoviesService(MoviesDbCotext dbContext)
         {
@@ -19,27 +19,44 @@
 
         public Response GetMovieById(int id)
         {
-            response.Content = JsonSerializer.Serialize(_dbContext.Movies.Select(x=>x.Id == id).ToList());
+            response.Content = JsonSerializer.Serialize(_dbContext.Movies.Where(x=>x.Id == id).SingleOrDefault());
             response.StatusCode = HttpStatusCode.OK;
-            return new Response();
+            return response;
         }
 
         public Response AddMovie(Movies movie)
         {
-            var result = _dbContext.Movies.Add(movie);
+            _dbContext.Movies.Add(movie);
             _dbContext.SaveChanges();
-
-            return new Response();
+            response.Content = JsonSerializer.Serialize(movie);
+            response.StatusCode = HttpStatusCode.OK;
+            return response;
         }
         
         public Response DeleteMovie(int id)
         {
-            return new Response();
+            Movies data = _dbContext.Movies.Where(x => x.Id == id).FirstOrDefault()!;
+            _dbContext.Movies.Remove(data);
+            _dbContext.SaveChanges();
+            response.Content = "Record deleted successfully";
+            response.StatusCode = HttpStatusCode.OK;
+            return response;
         }
 
         public Response UpdateMovie(int id, Movies movie)
         {
-            return new Response();
+            Movies movies = _dbContext.Movies.Where(_x => _x.Id == id).SingleOrDefault()!;
+            movies.Title=movie.Title;
+            movies.Description = movie.Description;
+            movies.Director = movie.Director;
+            movies.UpdatedDate = DateTime.Now;
+
+            _dbContext.Movies.Update(movies);
+            _dbContext.SaveChanges();
+
+            response.StatusCode = HttpStatusCode.OK;
+            response.Content = "Record updated successfully";
+            return response;
         }
     }
 }
